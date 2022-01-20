7636b4d9659d8bea45565a7e19f604360c3536e1
Andrej Stojkovikj <40241148+AndrejStojkovic@users.noreply.github.com>
2022-01-21 00:51:20 +0100

    Initial commit

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
-from selenium.webdriver.support.ui import WebDriverWait as wait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
 import time
 import json
 
+# letters to scrape
 letters = ['а', 'б', 'в', 'г', 'д', 'ѓ', 'е', 'ж', 'з', 'ѕ',
            'и', 'ј', 'к', 'л', 'љ', 'м', 'н', 'њ', 'о', 'п',
            'р', 'с', 'т', 'ќ', 'у', 'ф', 'х', 'ц', 'ч', 'џ', 'ш']
 
+# If you want to scrape only certain letters comment the array above and uncomment below
+# and add the letters you want to the array using Macedonian Keyboard support as above
+
+#letters = ['а', 'к', 'л']
+
+# Function used to scrape each letter (SPECIFIC TO THIS SITE, DON'T TOUCH)
+# The dictionary on this site doesn't display all the words from a certain letter,
+# Sso you need this function to go through every word from a letter
 def scrape_letter(browser, letter):
     ranges = Select(browser.find_element(By.XPATH, "//select[@name='ranges']"))
     
@@ -18,11 +24,10 @@ def scrape_letter(browser, letter):
         ranges = Select(browser.find_element(By.XPATH, "//select[@name='ranges']"))
         ranges.select_by_index(index)
         grab_words(browser, letter) 
-        
-    #time.sleep(2)
 
+# Function used to scrape each word (SPECIFIC TO THIS SITE, DON'T TOUCH)
 def grab_words(browser, letter):
-    lexems = Select(wait(browser, 20).until(EC.presence_of_element_located((By.XPATH, "//select[@name='lexems']"))))
+    lexems = Select(browser.find_element(By.XPATH, "//select[@name='lexems']"))
     words = lexems.options
 
     for word in words:
@@ -34,6 +39,7 @@ def grab_words(browser, letter):
                 
         print(w[0])
 
+# Main function
 def main():
     for letter in letters:
         browser = webdriver.Chrome()
